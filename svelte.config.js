7e90ef6d872c9ef081098e491ef3cda8736eed3f
hunogrammer <seungsh524@gmail.com>
2024-08-24 16:14:47 +0900

    fix: css not found

--- svelte.config.js
+++ svelte.config.js
@@ -10,6 +10,7 @@ const config = {
 		adapter: adapter(),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/multitools-hub' : '',
+			relative: false
 		}
 	},
 	preprocess: vitePreprocess()
