c2f4915a0a7cd38e8395848ff66eb4e2ccd01d4f
hunogrammer <seungsh524@gmail.com>
2024-08-28 10:09:58 +0900

    feat: 점심메뉴 추천기 seo를 위한 메타태그 추가

--- src/routes/lunch/+page.svelte
+++ src/routes/lunch/+page.svelte
@@ -3,8 +3,14 @@
 </script>
 
 <svelte:head>
-	<title>점심 메뉴 추천</title>
-	<meta name="description" content="점심 메뉴 추천기" />
+	<title>점심메뉴 추천기</title>
+	<meta name="description" content="점심메뉴 무작위로 추천하는 사이트" />
+	<meta name="canonical" content="https://hunogrammer.github.io/lunch" />
+	<meta name="robots" content="index,nofollow" />
+	<meta name="og:type" content="website" />
+	<meta name="og:title" content="점심메뉴 추천기" />
+	<meta name="og:url" content="https://hunogrammer.github.io/lunch" />
+	<meta name="og:description" content="점심메뉴 무작위로 추천하는 사이트" />
 </svelte:head>
 
 <div>
